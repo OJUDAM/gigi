443b4c9ba1b95f80ffe11c8c331804111bdf0baf
OJUDAM <dhwnkea@naver.com>
2023-06-20 17:13:20 +0900

    배포 테스트 2 (쉘 스크립트 인자 변경)

--- deploy-jenkins.sh
+++ deploy-jenkins.sh
@@ -4,7 +4,7 @@ if [ -z $1 ]
 then
     echo -e ">> 첫번째 인자가 비었습니다.\n"
     echo ">> 첫번째 인자값은 profile명 입니다."
-elif [ $1 != "dev-web" ] && [ $1 != "prod" ]; then
+elif [ $1 != "dev" ] && [ $1 != "prod" ]; then
     echo ">> 첫번째 인자 값은 dev(개발) or prod(운영)로 입력해주세요."
 else
     cd /var/lib/jenkins/workspace/gigi
