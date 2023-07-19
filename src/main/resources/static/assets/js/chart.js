8b2b9d5d23d4b1925bcf371b2848c54c5bc6928a
OJUDAM <dhwnkea@naver.com>
2023-07-19 15:30:35 +0900

    그래프 색상 변경

--- src/main/resources/static/assets/js/chart.js
+++ src/main/resources/static/assets/js/chart.js
@@ -18,7 +18,26 @@
                         data: data,
                         //borderColor: Utils.CHART_COLORS.red,
                         //backgroundColor: Utils.transparentize(Utils.CHART_COLORS.red, 0.5),
-                        backgroundColor: randomColor(labels)
+                       // backgroundColor: randomColor(labels)
+                        backgroundColor: [
+                           //색상
+                           'rgba(255, 99, 132, 0.2)',
+                           'rgba(54, 162, 235, 0.2)',
+                           'rgba(255, 206, 86, 0.2)',
+                           'rgba(75, 192, 192, 0.2)',
+                           'rgba(153, 102, 255, 0.2)',
+                           'rgba(255, 159, 64, 0.2)'
+                       ],
+                       borderColor: [
+                           //경계선 색상
+                           'rgba(255, 99, 132, 1)',
+                           'rgba(54, 162, 235, 1)',
+                           'rgba(255, 206, 86, 1)',
+                           'rgba(75, 192, 192, 1)',
+                           'rgba(153, 102, 255, 1)',
+                           'rgba(255, 159, 64, 1)'
+                       ],
+                       borderWidth: 1 //경계선 굵기
                     }]
                 },
                 options: {
