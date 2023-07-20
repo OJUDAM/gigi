2d8ff7942aec75446e02fbf9def5a37d3f71a04f
OJUDAM <dhwnkea@naver.com>
2023-07-20 12:09:57 +0900

    그래프 그룹 추가

--- src/main/resources/static/assets/js/chart.js
+++ src/main/resources/static/assets/js/chart.js
@@ -1,6 +1,23 @@
 
  function showChart(jsonData) {
-        //차트 색상 랜덤
+        //그래프 색상
+        var backColor = [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(255, 159, 64, 0.2)'
+            ]
+        //그래프 가장자리 색상
+        var borderColor=[
+           'rgba(255, 99, 132, 1)',
+           'rgba(54, 162, 235, 1)',
+           'rgba(255, 206, 86, 1)',
+           'rgba(75, 192, 192, 1)',
+           'rgba(153, 102, 255, 1)',
+           'rgba(255, 159, 64, 1)'
+           ]
         function randomColor(labels) {
             var colors = [];
             for (let i = 0; i < labels.length; i++) {
@@ -8,37 +25,54 @@
             }
             return colors;
         }
-        function makeChart(ctx, type, labels, data) {
+        function makeChart(ctx, type, labels, data00, data10, data20, data30, data40, data50) {
             var myChart = new Chart(ctx, {
                 type: type,
                 data: {
                     labels: labels,
                     datasets: [{
-                        label: '지하철 혼잡도',
-                        data: data,
-                        //borderColor: Utils.CHART_COLORS.red,
-                        //backgroundColor: Utils.transparentize(Utils.CHART_COLORS.red, 0.5),
-                       // backgroundColor: randomColor(labels)
-                        backgroundColor: [
-                           //색상
-                           'rgba(255, 99, 132, 0.2)',
-                           'rgba(54, 162, 235, 0.2)',
-                           'rgba(255, 206, 86, 0.2)',
-                           'rgba(75, 192, 192, 0.2)',
-                           'rgba(153, 102, 255, 0.2)',
-                           'rgba(255, 159, 64, 0.2)'
-                       ],
-                       borderColor: [
-                           //경계선 색상
-                           'rgba(255, 99, 132, 1)',
-                           'rgba(54, 162, 235, 1)',
-                           'rgba(255, 206, 86, 1)',
-                           'rgba(75, 192, 192, 1)',
-                           'rgba(153, 102, 255, 1)',
-                           'rgba(255, 159, 64, 1)'
-                       ],
+                        label: '00분',
+                        data: data00,
+                        backgroundColor: backColor[0],
+                       borderColor: borderColor[0],
                        borderWidth: 1 //경계선 굵기
-                    }]
+                    },
+                    {
+                        label: '10분',
+                        data: data10,
+                       backgroundColor: backColor[1],
+                       borderColor: borderColor[1],
+                       borderWidth: 1 //경계선 굵기
+                    },
+                    {
+                        label: '20분',
+                        data: data20,
+                       backgroundColor: backColor[2],
+                       borderColor: borderColor[2],
+                       borderWidth: 1 //경계선 굵기
+                    },
+                    {
+                         label: '30분',
+                         data: data30,
+                        backgroundColor: backColor[3],
+                        borderColor: borderColor[3],
+                        borderWidth: 1 //경계선 굵기
+                    },
+                     {
+                         label: '40분',
+                         data: data40,
+                        backgroundColor: backColor[4],
+                        borderColor: borderColor[4],
+                        borderWidth: 1 //경계선 굵기
+                    },
+                     {
+                         label: '50분',
+                         data: data50,
+                        backgroundColor: backColor[5],
+                        borderColor: borderColor[5],
+                        borderWidth: 1 //경계선 굵기
+                    }
+                    ]
                 },
                 options: {
                     responsive: true,
@@ -62,20 +96,35 @@
         var congestionData = convertJsonToObject(jsonData);
 
         var labelList = new Array();
-        var valueList = new Array();
-        var colorList = new Array();
+        var valueList00 = new Array();
+        var valueList10 = new Array();
+        var valueList20 = new Array();
+        var valueList30 = new Array();
+        var valueList40 = new Array();
+        var valueList50 = new Array();
 
         for(var i = 0; i<congestionData.length; i++) {
             var congestion = congestionData[i];
-            labelList.push(congestion.stationName);
-            valueList.push(congestion.congestionMin10);
+            labelList.push(congestion.stationName + '(' + congestion.hour+ '시)');
+            valueList00.push(congestion.congestionMin00);
+            valueList10.push(congestion.congestionMin10);
+            valueList20.push(congestion.congestionMin20);
+            valueList30.push(congestion.congestionMin30);
+            valueList40.push(congestion.congestionMin40);
+            valueList50.push(congestion.congestionMin50);
         }
 
         var newLabels = labelList.slice(-5);
-        var newMyData = valueList.slice(-5);
+        var newMyData00 = valueList00.slice(-5);
+        var newMyData10 = valueList10.slice(-5);
+        var newMyData20 = valueList20.slice(-5);
+        var newMyData30 = valueList30.slice(-5);
+        var newMyData40 = valueList40.slice(-5);
+        var newMyData50 = valueList50.slice(-5);
+
         // Chart.js 막대그래프 그리기
         var ctx = $('#myChart');
-        makeChart(ctx, 'bar', newLabels, newMyData);
+        makeChart(ctx, 'bar', newLabels, newMyData00, newMyData10, newMyData20, newMyData30, newMyData40, newMyData50);
 
        // Chart.js 선그래프 그리기
         //ctx = $('#myChart2');
