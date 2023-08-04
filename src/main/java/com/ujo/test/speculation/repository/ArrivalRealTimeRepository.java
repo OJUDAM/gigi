5a83de3f83aa08ac09e74ae75ea66cc3060da7c3
OJUDAM <dhwnkea@naver.com>
2023-08-04 18:50:52 +0900

    키 중복시 업데이트 컬럼 추가4

--- src/main/java/com/ujo/test/speculation/repository/ArrivalRealTimeRepository.java
+++ src/main/java/com/ujo/test/speculation/repository/ArrivalRealTimeRepository.java
@@ -10,14 +10,14 @@ public interface ArrivalRealTimeRepository {
 
     @Insert(" INSERT INTO STATION_ARRIVAL_REALTIME" +
             " ( TRAIN_NO, TRAIN_NAME, ARRIVAL_STATION_CODE" +
-            " , ARRIVAL_MESSAGE, TARGET_STATION_CODE, ARRVIAL_CODE" +
+            " , ARRIVAL_MESSAGE, TARGET_STATION_CODE, ARRIVAL_CODE" +
             " , DIRECT_AT, UP_DN_LINE, ARRIVAL_DATE,CREATED_AT )" +
             " VALUES" +
             " ( #{trainNo}, #{trainName}, #{arrivalStationCode}" +
             " , #{arrivalMessage}, #{targetStationCode}, #{arrivalCode}" +
             " , #{directAt}, #{upDnLine}, #{arrivalDate}, #{createdAt} )" +
             " ON DUPLICATE KEY UPDATE UPDATED_AT = NOW(), ARRIVAL_MESSAGE = #{arrivalMessage}" +
-            " , ARRIVAL_STATION_CODE = #{arrivalCode}, CREATED_AT = #{createdAt}")
+            " , ARRIVAL_STATION_CODE = #{arrivalStationCode}, ARRIVAL_CODE=#{arrivalCode}, CREATED_AT = #{createdAt}")
     int save(ArrivalRealTimeEntity arrivalRealTimeEntity);
 
 
