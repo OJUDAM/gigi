5720795cf70f933f572014a0c512381996628a15
OJUDAM <dhwnkea@naver.com>
2023-08-06 14:52:28 +0900

    실시작 도착 정보 17-19 3시간으로 변경

--- src/main/java/com/ujo/test/speculation/service/RealTimeService.java
+++ src/main/java/com/ujo/test/speculation/service/RealTimeService.java
@@ -1,14 +1,11 @@
 package com.ujo.test.speculation.service;
 
 import com.ujo.test.entity.ArrivalRealTimeEntity;
-import com.ujo.test.gigi.dto.response.CongestionResponseDTO;
 import com.ujo.test.speculation.dto.response.ArrivalRealTimeResponseDTO;
 import com.ujo.test.speculation.repository.ArrivalRealTimeRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
-import java.io.IOException;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -19,7 +16,7 @@ public class RealTimeService {
     private final ArrivalRealTimeRepository arrivalRealTimeRepository;
 
     public List<ArrivalRealTimeResponseDTO> getArrivalTimeList() {
-        List<ArrivalRealTimeEntity> arrivalRealTimeEntities = arrivalRealTimeRepository.findAll();
+        List<ArrivalRealTimeEntity> arrivalRealTimeEntities = arrivalRealTimeRepository.findToDay();
 
         return arrivalRealTimeEntities.stream().map(ArrivalRealTimeResponseDTO::new).collect(Collectors.toList());
     }
